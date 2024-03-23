7c7e60b6f4f3078ca38b160e175f59c126946803
garrettmichiels <michielsgarrett@gmail.com>
2024-03-23 18:06:14 -0400

    change files and add some route structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
+import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Users/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <HashRouter>
+      <Routes>
+          <Route path="/"         element={ <Navigate to="/Home"/>}/>
+          <Route path="/Login/*"  element={<Login/>}/>
+          <Route path="/Profile/*"  element={<h1>Profile</h1>}/>
+          <Route path="/Details/*"  element={<h1>Details</h1>}/>
+          <Route path="/Search/*"  element={<h1>Search</h1>}/>
+        </Routes>
+      </HashRouter>
+      </div>
+   );
 }
 
 export default App;
