7366aced47e377a4e5aa14eaaf68ea9bcf1d0311
Maggie Collette <collette.m@northeastern.edu>
2024-03-25 11:45:25 -0400

    added home nav to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-import Login from './Users/Login';
+import React from "react";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./Users/Login";
+import Home from "./ConnectNEU/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <HashRouter>
-      <Routes>
-          <Route path="/"         element={ <Navigate to="/Home"/>}/>
-          <Route path="/Login/*"  element={<Login/>}/>
-          <Route path="/Profile/*"  element={<h1>Profile</h1>}/>
-          <Route path="/Details/*"  element={<h1>Details</h1>}/>
-          <Route path="/Search/*"  element={<h1>Search</h1>}/>
-        </Routes>
-      </HashRouter>
-      </div>
-   );
+	return (
+		<div className="App">
+			<HashRouter>
+				<Routes>
+					<Route path="/" element={<Navigate to="/Home" />} />
+					<Route path="/Home/" element={<Home />} />
+					<Route path="/Login/*" element={<Login />} />
+					<Route path="/Profile/*" element={<h1>Profile</h1>} />
+					<Route path="/Details/*" element={<h1>Details</h1>} />
+					<Route path="/Search/*" element={<h1>Search</h1>} />
+				</Routes>
+			</HashRouter>
+		</div>
+	);
 }
 
 export default App;
