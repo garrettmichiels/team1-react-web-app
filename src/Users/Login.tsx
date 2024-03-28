95743e2a70e122f08a3e2780a0e18c7f7c81dde9
garrettmichiels <michielsgarrett@gmail.com>
2024-03-27 20:43:45 -0400

    add some more details

--- src/Users/Login.tsx
+++ src/Users/Login.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as userClient from "./Client";
 export default function Login() {
-  const [user, setUser] = useState({ username: "", password: "" });
+  const [user, setUser] = useState({ username: "", password: "", email: "", firstName: "", lastName: ""});
   const navigate = useNavigate();
   const login = async () => {
     try {
@@ -13,22 +13,31 @@ export default function Login() {
     }
   };
   return (
-    <div className="container">
-        <div className="row">
+    <div className="" style={{minHeight: "100vh"}}>
+        <div className="row align-content-center">
+          <div className="col">
+            <h1>Login</h1>
+          </div>
+          <div className="col">
+            <h2>Don't have an account?</h2>
+                <h2>Register Here</h2>
+            </div>
+          </div>
+        <div className="row align-content-center" style={{minHeight: "100vh"}}>
             <div className="col">
-                <h1>Login</h1>
+                
                 <input
                     onChange={(e) => setUser({ ...user, username: e.target.value })}
                     value={user.username}
                     type="text"
-                    className="form-control"
+                    className="form-control mt-2"
                     placeholder="Username"
                 />
                 <input
                     onChange={(e) => setUser({ ...user, password: e.target.value })}
                     value={user.password}
                     type="password"
-                    className="form-control"
+                    className="form-control mt-2"
                     placeholder="Password"
                 />
                 <br></br>
@@ -37,20 +46,37 @@ export default function Login() {
                 </button>
         </div>
             <div className="col">
-            <h2>Don't have an account?</h2>
-            <h2>Register Here</h2>
+              <h4 style={{textAlign: "left"}}>First Name</h4>
+            <input
+                onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+                value={user.firstName}
+                type="text"
+                className="form-control mt-2"
+                placeholder="First Name"
+            />
+            <h4 style={{textAlign: "left"}}>Last Name</h4>
+            <input
+                onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+                value={user.lastName}
+                type="text"
+                className="form-control mt-2"
+                placeholder="Last Name"
+            />
+
+            <h4 style={{textAlign: "left"}}>Username</h4>
             <input
                 onChange={(e) => setUser({ ...user, username: e.target.value })}
                 value={user.username}
                 type="text"
-                className="form-control"
+                className="form-control mt-2"
                 placeholder="Username"
             />
+            <h4 style={{textAlign: "left"}}>Password</h4>
             <input
                 onChange={(e) => setUser({ ...user, password: e.target.value })}
                 value={user.password}
                 type="password"
-                className="form-control"
+                className="form-control mt-2"
                 placeholder="Password"
             />
             <br></br>
