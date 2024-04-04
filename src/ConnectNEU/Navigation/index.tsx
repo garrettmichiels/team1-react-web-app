c404d2086b99cefbe2100a65e7261eae9974839e
Maggie Collette <collette.m@northeastern.edu>
2024-04-04 15:19:35 -0400

    update navbar

--- src/ConnectNEU/Navigation/index.tsx
+++ src/ConnectNEU/Navigation/index.tsx
@@ -4,9 +4,9 @@ export default function Navigation() {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 			<div className="container-fluid">
-				<a className="navbar-brand" href="#">
+				<Link to={"/Home"} className="navbar-brand">
 					ConnectNEU
-				</a>
+				</Link>
 				<button
 					className="navbar-toggler"
 					type="button"
@@ -20,47 +20,19 @@ export default function Navigation() {
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 						<li className="nav-item">
-							<Link to="/Home">Home</Link>
+							<Link to="/Home" className="nav-link">
+								Home
+							</Link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Link
-							</a>
-						</li>
-						<li className="nav-item dropdown">
-							<a
-								className="nav-link dropdown-toggle"
-								href="#"
-								role="button"
-								data-bs-toggle="dropdown"
-								aria-expanded="false">
-								Dropdown
-							</a>
-							<ul className="dropdown-menu">
-								<li>
-									<a className="dropdown-item" href="#">
-										Action
-									</a>
-								</li>
-								<li>
-									<a className="dropdown-item" href="#">
-										Another action
-									</a>
-								</li>
-								<li>
-									<hr className="dropdown-divider" />
-								</li>
-								<li>
-									<a className="dropdown-item" href="#">
-										Something else here
-									</a>
-								</li>
-							</ul>
+							<Link to={"/Profile"} className="nav-link">
+								Profile
+							</Link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link disabled" aria-disabled="true">
-								Disabled
-							</a>
+							<Link to={"/Account"} className="nav-link">
+								Account
+							</Link>
 						</li>
 					</ul>
 					<form className="d-flex" role="search">
